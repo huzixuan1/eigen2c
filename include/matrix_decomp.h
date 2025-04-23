2304ed0f2bceb80fe4d3b770ba997c4a3c884e59
TaoTao <45528733+huzixuan1@users.noreply.github.com>
2025-04-23 23:46:27 +0800

    Update matrix_decomp.h

--- include/matrix_decomp.h
+++ include/matrix_decomp.h
@@ -1,1 +1,12 @@
+#ifndef MATRIX_DECOMP_H
+#define MATRIX_DECOMP_H
+
+#include "matrix_ops.h"
+
+double matrix_LU_decomposition(const Matrix* A, Matrix* L, Matrix* U);
+double matrix_QR_decomposition(const Matrix* A, Matrix* Q, Matrix* R);
+double matrix_cholesky_decomposition(const Matrix* A, Matrix* L);
+double matrix_svd(const Matrix* A, Matrix* U, Matrix* S, Matrix* V);
+
+#endif  // MATRIX_DECOMP_H
 
